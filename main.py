8ad3671e5f8f5d78f3365cc8e91ee3d901f70657
jbprf <parfaitjeanbaptiste@gmail.com>
2024-09-27 16:28:20 +0200

    update : new features

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-etfs = ['SPY', 'QQQ', 'TQQQ']
+etfs = ['SPY', 'QQQ', 'TQQQ','AI.PA']
 
 
-data = yf.download(etfs, start='2000-01-01', end='2023-01-01')['Adj Close']
+data = yf.download(etfs, start='2015-01-01', end='2024-01-01')['Adj Close']
 
 
 print(data.head())
@@ -16,13 +17,24 @@ print(data.head())
 # Calculer les rendements quotidiens
 returns = data.pct_change()
 
-# Calculer les rendements cumulés
-cumulative_returns = (1 + returns).cumprod() - 1
-
-# Afficher les rendements cumulés
-print(cumulative_returns.tail())
 
 
 
-annual_returns = returns.mean() * 252
+
+
+annual_returns = returns.mean() * 252 * 100
 print("rendement annuel = " , annual_returns)
+
+
+# Calculer la matrice de corrélation
+correlation_matrix = returns.corr()
+
+# Afficher la matrice de corrélation avec une heatmap
+plt.figure(figsize=(8, 6))  # Ajuste la taille du graphique
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=0.5, vmin=-1, vmax=1)
+
+# Ajouter un titre
+plt.title('Matrice de corrélation des rendements', fontsize=16)
+
+# Afficher le graphique
+plt.show()
